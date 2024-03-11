08d3d5af2b265a7fbefb47045f0844bb080ee349
Avrilio Damar Maulana <97890325+avrilio@users.noreply.github.com>
2024-03-11 21:37:41 +0700

    Update Navbar.js

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -33,7 +33,7 @@ export default function Navbar() {
                     const Icon = socialMedia.Icon;
 
                     return (
-                        <li>
+                        <li key={index}>
                             <Link
                                 href={socialMedia.Link}
                                 aria-label={socialMedia.Label}
